d2ed7f3449c949ac86e8f74b05b9f6f57cfa83ca
empitsu <oneok0813@gmail.com>
2019-10-10 23:21:55 +0900

    update

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,28 +1,44 @@
 <template>
   <div>
-    <h1>Events Listing</h1>
-    <EventCard v-for="event in events" :key="event.id" :event="event" />
+    <h1>Events for {{ user.user.name }}</h1>
+    <EventCard v-for="event in event.events" :key="event.id" :event="event" />
+    <template v-if="page != 1">
+      <router-link
+        :to="{ name: 'event-list', query: { page: page - 1 } }"
+        rel="prev"
+        >Prev Page</router-link
+      >|
+    </template>
+    <router-link :to="{ name: 'event-list', query: { page: page + 1 } }"
+      >Next Page</router-link
+    >
   </div>
 </template>
 
 <script>
 import EventCard from '@/components/EventCard.vue'
-import EventService from '@/services/EventService.js'
 import { mapState } from 'vuex'
 
 export default {
   components: {
     EventCard
   },
-  data() {
-    return {
-      events: []
-    }
-  },
   created() {
-    this.$store.dispatch('fetchEvents')
+    this.$store.dispatch('fetchEvents', {
+      perPage: 3,
+      page: this.page
+    })
   },
-  computed: mapState(['events'])
+  computed: {
+    page() {
+      // What page we're currently on
+      return parseInt(this.$route.query.page) || 1
+    },
+    hasNextPage() {
+      return this.event.eventsTotal > this.page * this.perPage
+    },
+    ...mapState(['event', 'user'])
+  }
 }
 </script>
 
